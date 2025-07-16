718798063a4106d01d156c051b39e3380cbbcc20
Aryan <work.aryanshah16@gmail.com>
2025-07-16 15:17:52 +0530

    Update A8miniControl.c

--- A8miniControl.c
+++ A8miniControl.c
@@ -1,3 +1,33 @@
+/*
+ * Copyright (c) 2025 Aryan Shah
+ * Email: work.aryanshah16@gmail.com
+ *
+ * This file is part of a project licensed primarily under the MIT License.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * Additional components or dependencies in this project may be under
+ * different licenses (e.g., Apache-2.0, GPL-3.0, or CC BY 4.0). Please refer to
+ * the LICENSES folder or documentation for more details.
+ */
+
+
 #include "A8miniControl.h"
 
 int main(int argc, char *argv[]){
